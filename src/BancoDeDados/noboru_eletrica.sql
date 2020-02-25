4a4558d90da97112bdbf710ccb758fcafa99a18f
danie <danie@DESKTOP-8JQ17NM>
2020-02-24 22:01:50 -0300

    Ajuste do script sql

--- src/BancoDeDados/noboru_eletrica.sql
+++ src/BancoDeDados/noboru_eletrica.sql
@@ -1,19 +1,19 @@
 use noboru_eletrica;
 
 create table cliente (
-	idCliente 	INT 		 NOT NULL 	AUTO_INCREMENT,
-    nome		VARCHAR(150) NOT NULL,
-    cpf			VARCHAR(11)  NOT NULL,
+    idCliente           INT 		 NOT NULL 	AUTO_INCREMENT,
+    nome		VARCHAR(150)     NOT NULL,
+    cpf			VARCHAR(11)      NOT NULL,
     celular		VARCHAR(20)	 NULL,
-    email		VARCHAR(100) NULL,
+    email		VARCHAR(100)     NULL,
     ativo		TINYINT		 NOT NULL,
     PRIMARY KEY (idCliente)
 );
 
 create table tipo_de_atendimento (
-	idTipoDeAtendimento 	 INT 		  NOT NULL 	AUTO_INCREMENT,
-    nomeTipoDeAtendimento	 VARCHAR(150) NOT NULL,
-    ativo					 TINYINT	  NOT NULL,
+    idTipoDeAtendimento 	 INT 		  NOT NULL 	AUTO_INCREMENT,
+    nomeTipoDeAtendimento	 VARCHAR(150)     NOT NULL,
+    ativo			 TINYINT	  NOT NULL,
     PRIMARY KEY (idTipoDeAtendimento)
 );
 
